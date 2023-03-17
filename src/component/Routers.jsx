a1c6d0c67bf9b21f05bb38c3801011e5110abcc9
Harsh Davda <harsh.davda@iflair.com>
2023-03-17 15:18:47 +0530

    added Route for component

--- src/component/Routers.jsx
+++ src/component/Routers.jsx
@@ -1,5 +1,7 @@
 
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import components for router
 import DragAndDrop from './dragDropContent';
 import EmailContent from './emailContent';
 import DownloadContent from './download';
@@ -7,19 +9,26 @@ import GoogleMap from './googlemap';
 import PaymentGateWays from './paymentGateways';
 import SignatureCanvas from './signatureCanvas';
 import UpiIntegration from './upiIntegration';
-
-
-import {
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
+import PageNotFound from './pageNotFound';
+import Overview from './Overview';
 export default function Routers() {
 
-  const pageComponent = [<GoogleMap />, <SignatureCanvas />, <EmailContent />, <PaymentGateWays />,  <DownloadContent />, <UpiIntegration />, <DragAndDrop />]
-  const pageLink = ["/googlemap", "/signature-canvas", "/Email-functionality", "/payment-integration", "/downloadfiles", "/upi-integration", "/drag-and-drop"];
+  const pageComponent = [ <Overview/>,<Overview/>, <GoogleMap/>, <SignatureCanvas/>, <EmailContent/>, <PaymentGateWays/>, <DownloadContent/>, <UpiIntegration/>, <DragAndDrop />, <PageNotFound/>]
+  const pageLink = ["/","/overview","/googlemap", "/signature-canvas", "/Email-functionality", "/payment-integration", "/downloadfiles", "/upi-integration", "/drag-and-drop","*"];
 
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          {pageComponent?.map((list,i) => {
+            if(i=== 0){
+            return <Route exact path={pageLink[i]} element={list}></Route>
+          } else{
+            return <Route path={pageLink[i]} element={list}></Route>
+          }})}
+        </Routes>
+      </BrowserRouter>
+
     </div>
   )
 }
